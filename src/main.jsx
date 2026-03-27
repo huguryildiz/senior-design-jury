1c0df17d4b1220762aa072e4e94aeed4ca9a79c7
huguryildiz <103906958+huguryildiz@users.noreply.github.com>
2026-03-27 23:58:16 +0300

    feat: initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ToastProvider } from "./components/toast/useToast";
 import ToastContainer from "./components/toast/ToastContainer";
+import "./styles/globals.css";
 import "./styles/shared.css";
 import "./styles/toast.css";
 
